ba712dfc5edb8765f9a611766e4152d6f17f0a77
Dominick Weston <DominickSWeston@outlook.com>
2025-03-06 10:25:53 -0600

    added skeleton code

---
+++ Medium/Add Two Numbers/add_two_numbers.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+
+struct ListNode {
+     int val;
+     ListNode *next; // ptr to next node
+     ListNode() : val(0), next(nullptr) {} // 
+     ListNode(int x) : val(x), next(nullptr) {} // 
+     ListNode(int x, ListNode *next) : val(x), next(next) {} // 
+ };
+
+class Solution {
+    public:
+        ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+            
+        }
+    };
