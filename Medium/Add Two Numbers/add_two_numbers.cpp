0e00d9ff6b83832c162a832398660f778050b358
Dominick Weston <DominickSWeston@outlook.com>
2025-03-06 10:26:47 -0600

    comment for constructors

--- Medium/Add Two Numbers/add_two_numbers.cpp
+++ Medium/Add Two Numbers/add_two_numbers.cpp
@@ -3,9 +3,11 @@ using namespace std;
 struct ListNode {
      int val;
      ListNode *next; // ptr to next node
-     ListNode() : val(0), next(nullptr) {} // 
-     ListNode(int x) : val(x), next(nullptr) {} // 
-     ListNode(int x, ListNode *next) : val(x), next(next) {} // 
+
+     // constructors
+     ListNode() : val(0), next(nullptr) {} 
+     ListNode(int x) : val(x), next(nullptr) {} 
+     ListNode(int x, ListNode *next) : val(x), next(next) {} 
  };
 
 class Solution {
