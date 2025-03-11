1dfe11009bc67341f45c5d9d3f289859c70ce349
Dominick Weston <DominickSWeston@outlook.com>
2025-03-11 16:08:47 -0500

    added comments

--- Easy/Fibonacci Number/FibonacciNumberSolution.cpp
+++ Easy/Fibonacci Number/FibonacciNumberSolution.cpp
@@ -1,13 +1,13 @@
 #include <vector>
 using namespace std;
 
-std::vector<int> dp(1000, -1);
+std::vector<int> dp(1000, -1); // large array w -1 (not yet computed)
 
 class Solution {
     public:
         int fib(int n) {
             if ( n <= 1) return n;
-            if (dp[n] != -1) return dp[n];
+            if (dp[n] != -1) return dp[n]; // if already solved, return stored answer
             return dp[n] = fib(n-1) + fib(n-2);
         }
     };
