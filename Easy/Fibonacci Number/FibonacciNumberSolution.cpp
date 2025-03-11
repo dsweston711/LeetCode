fa411eee4ec8733208cbe228c55aa0d18d1e857f
Dominick Weston <DominickSWeston@outlook.com>
2025-03-11 16:07:32 -0500

    solved via dynamic programming

--- Easy/Fibonacci Number/FibonacciNumberSolution.cpp
+++ Easy/Fibonacci Number/FibonacciNumberSolution.cpp
@@ -1,7 +1,13 @@
+#include <vector>
+using namespace std;
+
+std::vector<int> dp(1000, -1);
+
 class Solution {
     public:
         int fib(int n) {
             if ( n <= 1) return n;
-            return fib(n-1) + fib(n-2);
+            if (dp[n] != -1) return dp[n];
+            return dp[n] = fib(n-1) + fib(n-2);
         }
     };
