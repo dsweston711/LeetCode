98683f4df353a89dea32d8ebc7ad3b1ca7586e0a
Dominick Weston <DominickSWeston@outlook.com>
2025-03-14 14:53:14 -0500

    finished adding two numbers problem

--- Medium/Add Two Numbers/README.md
+++ Medium/Add Two Numbers/README.md
@@ -7,3 +7,15 @@
     - return 0?
     - for each call, increase multiplication factor
         - 1, 10, 100, 1000, &c.
+
+After a couple days, perhaps...
+
+ill just traverse the linked lists while the head isnt null and multiply each value by prevplace*10.
+
+then add the numbers.
+
+currently cant think of a better solution, given we dont know the size and rather than reversing the list linked with a stack directly grabbing each value seems less time-consuming. I could be wrong, though.
+
+just checked the desired output... again. ive been approaching this wrong. we directly compare each value since both lists are in reverse and the output will be in reverse, construct the list as we go. this will probably be done recursively.
+
+addendum: after letting it roll around in my head for a while, i decided to use a recursive function. qutie happy with this implementation. comments in the code are sufficient i believe.
--- Medium/Add Two Numbers/add_two_numbers.cpp
+++ Medium/Add Two Numbers/add_two_numbers.cpp
@@ -1,32 +1,54 @@
+#include <vector>
+#include <iostream>
 using namespace std;
 
-struct ListNode {
-     int val; // value of node
-     ListNode *next; // ptr to next node
+struct ListNode
+{
+    int val;        // value of node
+    ListNode *next; // ptr to next node
 
-     // constructors
-     ListNode() : val(0), next(nullptr) {} 
-     ListNode(int x) : val(x), next(nullptr) {} 
-     ListNode(int x, ListNode *next) : val(x), next(next) {} 
- };
+    // constructors
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
-class Solution {
+class Solution
+{
 
-    int addTwoNumbersRecursiveFunction(int factor, ListNode* currentNode) {
+    // recursive function to add two numbers represented by linked lists
+    ListNode *addTwoNumbersModified(ListNode *headOne, ListNode *headTwo, int carry)
+    {
+        // base case: If both lists are empty and no carry, return nullptr
+        if (!headOne && !headTwo && carry == 0)
+        {
+            return nullptr;
+        }
 
-        ListNode *nextNode = currentNode->next; // ptr to next node
+        // get values or use 0 if node is null
+        int valOne = (headOne) ? headOne->val : 0;
+        int valTwo = (headTwo) ? headTwo->val : 0;
 
-        //TODO
-        // base case : next node is null (check pointer?)
-        if (nextNode == nullptr) { return currentNode->val; }
+        // compute sum and carry
+        int sum = valOne + valTwo + carry;
+        int newCarry = sum / 10; // carry for next recursion
+        int digit = sum % 10;    // single digit for this node
 
-        return currentNode->val + addTwoNumbersRecursiveFunction((factor*10), nextNode);
+        // create new node for the result
+        ListNode *resultNode = new ListNode(digit);
+
+        // recursive call to get the next node
+        resultNode->next = addTwoNumbersModified(
+            (headOne) ? headOne->next : nullptr,
+            (headTwo) ? headTwo->next : nullptr,
+            newCarry);
+
+        return resultNode;
     }
 
-    public:
-        ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-            int x = addTwoNumbersRecursiveFunction(1, l1);
-            int y = addTwoNumbersRecursiveFunction(1, l2);
-
-        }
-    };
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        return addTwoNumbersModified(l1, l2, 0); // start recursion with carry = 0
+    }
+};
