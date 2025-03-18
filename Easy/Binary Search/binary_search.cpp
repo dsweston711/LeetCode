9d53beedb99cb61cbc08edbb51e4f3ce036119fc
Dominick Weston <DominickSWeston@outlook.com>
2025-03-18 16:58:14 -0500

    updated makefiles, readmes, and added basic binary search problem (start with the basics!)

--- Easy/Binary Search/binary_search.cpp
+++ Easy/Binary Search/binary_search.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Solution
 {
 public:
-    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+    int search(vector<int> &nums, int target)
     {
     }
 };
