49afceeea3b8574d95ea1c2f8e1807b079597745
Dominick Weston <DominickSWeston@outlook.com>
2025-03-23 21:38:06 -0500

    finished longest common prefix problem

--- Easy/Longest Common Prefix/longest_common_prefix.cpp
+++ Easy/Longest Common Prefix/longest_common_prefix.cpp
@@ -4,18 +4,43 @@
 
 using namespace std;
 
-class Solution {
-    public:
-        string longestCommonPrefix(vector<string>& strs) {
-            for (string str : strs) {
-                cout << str << endl;
-            }
+class Solution
+{
+public:
+    string getPrefix(const string &a, const string &b)
+    {
+        int i = 0;
+        while (i < (int)a.size() && i < (int)b.size() && a[i] == b[i])
+        {
+            ++i;
         }
-    };
-
-    int main() {
-        vector<string> strs = {"flower", "flow","flight"};
-        Solution sol;
-        sol.longestCommonPrefix(strs);
-        return 0;
+        return a.substr(0, i);
     }
+
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        vector<string> dp(strs.size());
+
+        for (string str : strs)
+        {
+            cout << str << endl;
+        }
+
+        dp[0] = strs[0];
+        cout << dp[0] << endl;
+
+        for (int i = 1; i < (int)strs.size(); ++i)
+        {
+            dp[i] = getPrefix(dp[i - 1], strs[i]);
+        }
+        return dp[strs.size() - 1];
+    }
+};
+
+int main()
+{
+    vector<string> strs = {"flower", "flow", "flight"};
+    Solution sol;
+    cout << sol.longestCommonPrefix(strs) << endl;
+    return 0;
+}
