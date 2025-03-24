fcdd4ab87f34e77959b027b4a233eb3171265392
Dominick Weston <DominickSWeston@outlook.com>
2025-03-23 22:37:18 -0500

    comments for explanation/learning

--- Medium/Length of the Longest Common Prefix/length_of_longest_common_prefix.cc
+++ Medium/Length of the Longest Common Prefix/length_of_longest_common_prefix.cc
@@ -10,35 +10,46 @@ class Solution
 public:
     int longestCommonPrefix(vector<int> &arr1, vector<int> &arr2)
     {
+        // create a hash set to store all prefixes of numbers in arr2
         unordered_set<string> prefixSet;
 
-        // store all prefixes from arr2
+        // for each number in arr2
         for (int num : arr2)
         {
-            string s = to_string(num);
+            string s = to_string(num); // convert the number to a string to access digits easily
             string prefix = "";
+
+            // build all prefixes of the number
             for (char c : s)
             {
-                prefix += c;
-                prefixSet.insert(prefix);
+                prefix += c;              // add one digit at a time
+                prefixSet.insert(prefix); // insert the current prefix into the set
             }
         }
-        // check arr1 prefixes
+
+        // keep track of the maximum matching prefix length found
         int maxPrefixLen = 0;
 
         for (int num : arr1)
         {
-            string s = to_string(num);
+            string s = to_string(num); // convery the number to a string
             string prefix = "";
+
+            // build prefixes from left to right
             for (int i = 0; i < (int)s.size(); ++i)
             {
-                prefix += s[i];
+                prefix += s[i]; // add next digit to prefix
+
+                // if the current prefix is found in arr2's prefix set, it's a match
                 if (prefixSet.count(prefix))
                 {
                     maxPrefixLen = max(maxPrefixLen, (int)prefix.size());
                 }
+                // otherwise, we keep checking shorter prefixes (we don't break yet)
             }
         }
+
+        // return the length of the longest common prefix found between any pair
         return maxPrefixLen;
     }
 };
