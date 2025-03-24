80163b485a2f3def2809269f864e96b2d1862c88
Dominick Weston <DominickSWeston@outlook.com>
2025-03-23 22:28:40 -0500

    finished longest common prefix problem

--- Medium/Length of the Longest Common Prefix/README.md
+++ Medium/Length of the Longest Common Prefix/README.md
@@ -7,3 +7,15 @@ this problem seems very similar to the previous easy problem, so i'll be giving 
 given the two integers from `arr1` and `arr2`, the prefix is formed from its leftmost digit.
 
 this doesnt seem to be an issue at first, as for each check from the last prolem we iterate from the beginning.
+
+---
+
+i think this might actually warrant a dp table, wherein
+
+`dp[i][j] = longest common prefix length up to strings i and j`
+
+nevermind, we dont build off of previous calculations lol
+
+---
+
+greedy explodes... so... perhaps a hashtable?
--- Medium/Length of the Longest Common Prefix/length_of_longest_common_prefix.cc
+++ Medium/Length of the Longest Common Prefix/length_of_longest_common_prefix.cc
@@ -1,5 +1,7 @@
 #include <vector>
 #include <iostream>
+#include <string>
+#include <unordered_set>
 
 using namespace std;
 
@@ -8,5 +10,46 @@ class Solution
 public:
     int longestCommonPrefix(vector<int> &arr1, vector<int> &arr2)
     {
+        unordered_set<string> prefixSet;
+
+        // store all prefixes from arr2
+        for (int num : arr2)
+        {
+            string s = to_string(num);
+            string prefix = "";
+            for (char c : s)
+            {
+                prefix += c;
+                prefixSet.insert(prefix);
+            }
+        }
+        // check arr1 prefixes
+        int maxPrefixLen = 0;
+
+        for (int num : arr1)
+        {
+            string s = to_string(num);
+            string prefix = "";
+            for (int i = 0; i < (int)s.size(); ++i)
+            {
+                prefix += s[i];
+                if (prefixSet.count(prefix))
+                {
+                    maxPrefixLen = max(maxPrefixLen, (int)prefix.size());
+                }
+            }
+        }
+        return maxPrefixLen;
     }
 };
+
+int main()
+{
+    vector<int> arr1 = {12345, 12467, 99999};
+    vector<int> arr2 = {12400, 12000, 12999};
+
+    Solution sol;
+    int res = sol.longestCommonPrefix(arr1, arr2);
+    cout << res << endl;
+    return 0;
+}
