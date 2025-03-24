6d0f38a0a69d2b7f5e113a3904e9f723d642dc5b
Dominick Weston <DominickSWeston@outlook.com>
2025-03-24 01:11:28 -0500

    first solution to average problem

--- Easy/1491. Average Salary Excluding the Minimum and Maximum Salary/README.md
+++ Easy/1491. Average Salary Excluding the Minimum and Maximum Salary/README.md
@@ -1,1 +1,15 @@
 # Easy - 1491. Average Salary Excluding the Minimum and Maximum Salary
+
+This problem is rather straightforward. Given an array of unique salaries, return the average of the array sans the minimum and maximum of the array.
+
+The first thought that came to mind when finding a min/max is using a binary search. I've come to love this algorithm, however it's unnecessary. A single pass with suffice.
+
+The average will be the total sans the min/max divded by `n - 2`. Or:
+
+`(total - min - max) / (n - 2)`
+
+---
+
+# Afterward
+
+After looking at the fastest solution, I see where improvement can be made. Sort the list -> iterate from 1 to size-1 -> perform calculation.
--- Easy/1491. Average Salary Excluding the Minimum and Maximum Salary/main.cc
+++ Easy/1491. Average Salary Excluding the Minimum and Maximum Salary/main.cc
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -7,5 +8,49 @@ class Solution
 public:
     double average(vector<int> &salary)
     {
+        double calculated_average = 0;    // calculated average
+        int min = INT_MAX;                // minimum value found
+        int max = INT_MIN;                // maximum value found
+        double total = 0;                 // sum of all values
+        double n = (double)salary.size(); // size of array
+
+        // iterate once over salaries
+        for (int i = 0; i < (int)salary.size(); i++)
+        {
+            // check for minimum and maximum
+            if (salary[i] < min)
+            {
+                min = salary[i];
+            }
+
+            if (salary[i] > max)
+            {
+                max = salary[i];
+            }
+
+            // sum all values
+            total += salary[i];
+            cout << total << endl;
+        }
+
+        cout << max << endl;
+
+        cout << min << endl;
+
+        total = total - min - max;
+
+        cout << total << endl;
+
+        calculated_average = total / (n - 2);
+
+        return calculated_average;
     }
 };
+
+int main()
+{
+    vector<int> salary = {4000, 3000, 1000, 2000};
+    Solution sol;
+    cout << sol.average(salary) << endl;
+    return 0;
+}
