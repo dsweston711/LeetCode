7b9b5b7aa01d99f643f04d208f2a392b44c42ff4
Dominick Weston <DominickSWeston@outlook.com>
2025-05-23 10:47:36 -0500

    finished problem

--- Hard/4. Median of Two Sorted Arrays/README.md
+++ Hard/4. Median of Two Sorted Arrays/README.md
@@ -1,3 +1,55 @@
 # Hard - Median of Two Sorted Arrays
+---
+Runtime of O(log (m+n)). We don't need to make the new merged array - we just need to find the median.
 
-TODO: Explanation to be added.
+So we can have odd-length medians:
+
+```[1,3], [2]
+median is 2```
+
+even-length medians:
+
+```[1,2], [3,4]
+(2+3)/2 = 2.5```
+
+uneven lengths:
+
+```[1,3,5,7], [2,4]
+(3+4)/2 = 3.5```
+
+one empty array:
+```[], [2,3]
+(2+3)/2 = 2.5```
+
+duplicate values:
+```[1,1], [1,1]
+1```
+
+negative values:
+```[-5,3,6], [-2,4,10]
+(3+4)/2 = 3.5```
+
+one array fully smaller than the other:
+```[1,2,3], [100,101,102]
+(3+100)/2 = 51.5```
+
+arrays with different lengths and spacing:
+```[1,2,5,8,11], [3,4,6,7]
+5```
+
+my instincts are telling me this a binary search on a particular partition. that is:
+
+1. you binary search on the shorter array
+2. at each step, you choose a cut point `i` in the shorter array, which implies a cut point `j` in the other array
+```i + j = (total length + 1)```
+3. then you check if the largest elements on the left of the shorter array and the longer array is <= the smallest on the right in both
+4. if not, adjust the binary search: shift left or right depending on the inequality
+
+```text
+Partition:
+nums1: |----left----| i |----right----|
+nums2: |--------left--------| j |--------right--------|
+
+Condition:
+    maxLeftX <= minRightY AND maxLeftY <= minRightX
+```
--- Hard/4. Median of Two Sorted Arrays/median_of_two_sorted_arrays.cpp
+++ Hard/4. Median of Two Sorted Arrays/median_of_two_sorted_arrays.cpp
@@ -1,11 +1,105 @@
 #include <vector>
+#include <climits>
 
 using namespace std;
 
 class Solution
 {
 public:
+    double binarySearch(vector<int> &A, vector<int> &B, int total_length)
+    {
+        int m = A.size();
+        int n = B.size();
+
+        int low = 0, high = m;
+
+        while (low <= high)
+        {
+            int i = (low + high) / 2;
+            int j = (total_length + 1) / 2 - i;
+
+            int A_left = (i == 0) ? INT_MIN : A[i - 1];
+            int A_right = (i == m) ? INT_MAX : A[i];
+            int B_left = (j == 0) ? INT_MIN : B[j - 1];
+            int B_right = (j == n) ? INT_MAX : B[j];
+
+            if (A_left <= B_right && B_left <= A_right)
+            {
+                if (total_length % 2 == 0)
+                {
+                    return (max(A_left, B_left) + min(A_right, B_right)) / 2.0;
+                }
+                else
+                {
+                    return max(A_left, B_left);
+                }
+            }
+            else if (A_left > B_right)
+            {
+                high = i - 1;
+            }
+            else
+            {
+                low = i + 1;
+            }
+        }
+
+        return 0.0; // should never reach here
+    }
+
     double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
     {
+        int nums1_length = (int)nums1.size();
+        int nums2_length = (int)nums2.size();
+        int total_length = nums1_length + nums2_length;
+        double median;
+
+        // base case: both arrays are empty
+        if (total_length == 0)
+        {
+            return 0;
+        }
+
+        // base case: one array is empty
+        // return the median of the other array
+        if (nums1_length == 0)
+        {
+            if (nums2_length % 2 == 0) // even
+            {
+                int mid = nums2_length / 2;
+                median = (nums2[mid - 1] + nums2[mid]) / 2.0;
+                return median;
+            }
+            else // odd
+            {
+                median = nums2[nums2_length / 2];
+                return median;
+            }
+        }
+        else if (nums2_length == 0)
+        {
+            if (nums1_length % 2 == 0) // even
+            {
+                int mid = nums1_length / 2;
+                median = (nums1[mid - 1] + nums1[mid]) / 2.0;
+                return median;
+            }
+            else // odd
+            {
+                median = nums1[nums1_length / 2];
+                return median;
+            }
+        }
+
+        // binary search of partition
+        // first parameter is smaller, second is larger
+        if (nums1_length <= nums2_length)
+        {
+            return binarySearch(nums1, nums2, total_length);
+        }
+        else
+        {
+            return binarySearch(nums2, nums1, total_length);
+        }
     }
 };
